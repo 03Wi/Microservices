40faeacfb3c3ca9a2ce881b73df25091c59da8d5
0303 <wilson.osorio@uptc.edu.co>
2024-06-24 16:36:47 -0400

    Organizate of project

---
+++ microservices-gateway/src/main/java/com/microservices/microservicesgateway/MicroservicesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.microservicesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesGatewayApplication.class, args);
+    }
+
+}
